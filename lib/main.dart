1ed4898754b2f7e8bed8d8b74be9eeab5727610d
Daniel Diocis <danieldiocis@gmail.com>
2025-04-07 16:05:50 +0200

    funziona

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,16 +22,30 @@ class MusicPlayer extends StatefulWidget {
 
 class _MusicPlayerState extends State<MusicPlayer> {
   late AudioPlayer _player;
+  final List<Map<String, String>> songs = [
+    {"title": "Musica 1", "asset": "assets/audio/musica1.mp3"},
+    {"title": "Musica 2", "asset": "assets/audio/musica2.mp3"},
+    {"title": "Musica 3", "asset": "assets/audio/musica3.mp3"},
+  ];
+  String? _currentlyPlaying;
 
   @override
   void initState() {
     super.initState();
     _player = AudioPlayer();
+  }
 
-    // Puoi caricare un file remoto oppure locale
-    _player.setUrl(
-      "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
-    );
+  Future<void> playSong(String assetPath, String filename) async {
+    try {
+      final path = await loadAssetToFile(assetPath, filename);
+      await _player.setFilePath(path);
+      await _player.play();
+      setState(() {
+        _currentlyPlaying = filename;
+      });
+    } catch (e) {
+      print("Errore nel caricamento: $e");
+    }
   }
 
   @override
@@ -40,39 +57,39 @@ class _MusicPlayerState extends State<MusicPlayer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Lettore musicale")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            StreamBuilder<PlayerState>(
-              stream: _player.playerStateStream,
-              builder: (context, snapshot) {
-                final playerState = snapshot.data;
-                final playing = playerState?.playing;
-                final processingState = playerState?.processingState;
-
-                if (processingState == ProcessingState.loading ||
-                    processingState == ProcessingState.buffering) {
-                  return CircularProgressIndicator();
-                } else if (playing != true) {
-                  return IconButton(
-                    icon: Icon(Icons.play_arrow),
-                    iconSize: 64.0,
-                    onPressed: _player.play,
-                  );
-                } else {
-                  return IconButton(
-                    icon: Icon(Icons.pause),
-                    iconSize: 64.0,
-                    onPressed: _player.pause,
-                  );
-                }
+      appBar: AppBar(title: Text("VoidTracks 🎵")),
+      body: Column(
+        children: [
+          if (_currentlyPlaying != null)
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text("🎧 In riproduzione: $_currentlyPlaying"),
+            ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: songs.length,
+              itemBuilder: (context, index) {
+                final song = songs[index];
+                return ListTile(
+                  title: Text(song['title']!),
+                  onTap: () => playSong(song['asset']!, song['asset']!.split('/').last),
+                  trailing: Icon(Icons.play_arrow),
+                );
               },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+}
+
+/// Copia l’asset nel file system locale per essere riprodotto
+Future<String> loadAssetToFile(String assetPath, String filename) async {
+  final byteData = await rootBundle.load(assetPath);
+  final dir = await getApplicationDocumentsDirectory();
+  final file = File('${dir.path}/$filename');
+
+  await file.writeAsBytes(byteData.buffer.asUint8List(), flush: true);
+  return file.path;
 }
