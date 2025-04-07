2fc5a8f14cdfdd89ebfdb85b1e53eb7aee2a1a34
Daniel Diocis <danieldiocis@gmail.com>
2025-04-07 10:52:44 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Void Tracks',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Void Tracks'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
