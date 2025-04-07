ac4cefd108f811a28e9e3b4fe1053e5fff32d4cf
Daniel Diocis <danieldiocis@gmail.com>
2025-04-07 11:41:20 +0200

    2 robe

---
+++ lib/models/track.dart
@@ -0,0 +1,11 @@
+class Track {
+  final String title;
+  final String path;
+  final Duration? duration;
+
+  Track({
+    required this.title,
+    required this.path,
+    this.duration,
+  });
+}
